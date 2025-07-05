1e492c6f7d17f7e488df0d4c85db3c303c7dacf0
Vishal Kumar <vishalkumar240107@gmail.com>
2025-07-05 21:04:45 +0530

    add lines to system.py

--- system.py
+++ system.py
@@ -1,30 +1,9 @@
-class Item:
-    # print("I am called!!")
-    price_discount = 0.8
-    all = []
+from item import Item
+from phone import Phone
 
-    def __init__(self, name: str, price: float, quantity=0):
-        assert price >= 0, f"price({price}) is not acceptable!!"
-        assert quantity >= 0, f"quantity({quantity}) is not acceptable!!"
+p1 = Phone("Phone1", 100, 5, 3)
+p2 = Phone("Phone2", 200, 3, 2)
+p3 = Phone("Phone3", 500, 2, 1)
 
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-
-        Item.all.append(self)
-
-    def __str__(self):
-        return(self.name)
-    def __repr__(self):
-        return (f"Item ({self.name},{self.price}, {self.quantity})")
-
-    def total_price(self):
-        return self.price * self.quantity
-    def priceAfterDiscount(self):
-        return self.price * self.price_discount
-
-i1 = Item('Phone', 1000, 4)
-i2 = Item('Laptop', 10000, 2)
-
-print(Item.all)
-print(i1.all)
+print(p1, p2, p3)
+print(p1.all)
